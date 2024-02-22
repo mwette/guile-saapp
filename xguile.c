410136661d7d4dac55d3b820d5737726ea3538ef
Matthew Wette <mwette@alumni.caltech.edu>
2024-02-22 10:24:24 -0800

    may have things working; ready to cleanup

--- xguile.c
+++ xguile.c
@@ -4,18 +4,27 @@
 #include <stdlib.h>
 #include <libguile.h>
 
-extern void *_binary_dummy1_go_start
-	__attribute__((section(".data")));
-extern size_t *_binary_dummy1_go_size
-	__attribute__((section(".data")));
+//extern char* _binary_dummy1_go_start[_binary_dummy1_go_size];
+//extern char _binary_dummy1_go_start[100];
+extern char _binary_dummy1_go_start[];
+extern char _binary_dummy1_go_end[];
+
+char *abc = "abc";
 
 static void kont(void *closure, int argc, char **argv) {
-  char *ptr;
+  char *ptr, *end;
   size_t siz;
   SCM mem, res;
   SCM proc;
 
   ptr = _binary_dummy1_go_start;
+  end = _binary_dummy1_go_end;
+  siz = end - ptr;
+#if 0
+  ptr = _binary_dummy1_go_start;
+  end = _binary_dummy1_go_end;
+  siz = _binary_dummy1_go_end - _binary_dummy1_go_start;
+
   siz = *_binary_dummy1_go_size;
   mem = scm_c_take_typed_bytevector(ptr, siz, SCM_ARRAY_ELEMENT_TYPE_U8, 
                                     SCM_BOOL_F);
@@ -24,6 +33,7 @@ static void kont(void *closure, int argc, char **argv) {
   proc = scm_c_public_ref("(system vm loader)", "load-thunk-from-memory");
   res = scm_call_1(proc, mem);
 
+#endif
   scm_shell(argc, argv);
 }
 
