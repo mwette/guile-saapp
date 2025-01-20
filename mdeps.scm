105227c874c996a606d2ec92de585ba09a8c3cca
Matthew Wette <mwette@alumni.caltech.edu>
2025-01-20 09:34:07 -0800

    closer

--- mdeps.scm
+++ mdeps.scm
@@ -1,10 +1,17 @@
 ;; module dependencies
 
 (use-modules (ice-9 match))
-(use-modules ((srfi srfi-1) #:select (fold delete-duplicates)))
+(use-modules ((srfi srfi-1) #:select (fold every lset-union)))
+(use-modules ((system base compile) #:select (compiled-file-name)))
 (use-modules (ice-9 pretty-print))
 (define pp pretty-print)
 (define (sf fmt . args) (apply simple-format #t fmt args))
+
+(define ignore-me
+  '((system syntax internal)
+    (compile compile-file)
+    (guile-user) ;; ???
+    ))
 
 (define (module-filename mod-spec)
   (fold
@@ -18,17 +25,16 @@
    #f %load-path))
 
 (define (spec-dep spec seed)
-  (cons
-   (match spec
-     (((sdl ...) . _) sdl)
-     ((sdl ...) sdl))
-   seed))
+  (let ((sdl (match spec (((sdl ...) . _0) sdl) ((sdl ...) sdl))))
+    (if (member sdl ignore-me) seed (cons sdl seed))))
 
 (define (mod-deps exp seed)
   (let loop ((deps seed) (tail (cddr exp)))
     (match tail
       ('() deps)
       (`(#:use-module ,spec . ,rest)
+       (loop (spec-dep spec deps) rest))
+      (`(#:autoload ,spec ,procs . ,rest)
        (loop (spec-dep spec deps) rest))
       ((key val . rest)
        (loop deps rest)))))
@@ -39,17 +45,14 @@
       (let loop ((deps '()) (exp (read port)))
 	(match exp
 	  ((? eof-object?) (reverse deps))
-          (`(define-module . ,_)
-	   (loop (mod-deps exp deps) (read port)))
-          (`(use-modules . ,specs)
-           (loop (fold spec-dep deps specs) (read port)))
-          (_
-           (loop deps (read port))))))))
+          (`(define-module . ,_0) (loop (mod-deps exp deps) (read port)))
+          (`(use-modules . ,specs) (loop (fold spec-dep deps specs) (read port)))
+          (__ (loop deps (read port))))))))
 
 ;; mod-name : '(ice-9 regex)
 (define (probe-module mod-name)
   (sf "probe ~S => ~S\n" mod-name (module-filename mod-name))
-  (sleep 1)
+  ;;(sleep 1)
   (cons mod-name (probe-module-file (module-filename mod-name))))
 
 (define (get-dict mod-name)
@@ -63,6 +66,7 @@
       (let ((entry (probe-module (car todo))))
         (loop (cons entry dict) (append (cdr entry) todo)))))))
 
+#|
 (define (get-conns dict)
   (let loop ((cns '()) (dis dict))
     (if (null? dis) cns
@@ -97,31 +101,47 @@
         (loop (acons nd ix pre) rpost lks (1+ ix) jx nd conns stk uvs)))
      (else
       (values pre (acons nd jx rpost) lks)))))
+|#
 
+(define (tsort filed filel)
+  (define (covered? deps done) (every (lambda (e) (member e done)) deps))
+  (let loop ((done '()) (hd '()) (tl filel))
+    (if (null? tl)
+        (if (null? hd) done (loop done '() hd))
+        (cond
+         ((not (assq-ref filed (car tl)))
+          (loop (cons (car tl) done) hd (cdr tl)))
+         ((covered? (assq-ref filed (car tl)) done)
+          (loop (cons (car tl) done) hd (cdr tl)))
+         (else
+          (loop done (cons (car tl) hd) (cdr tl)))))))
 
 ;;(pp (get-dict '(nyacc lang c99 munge)))
 
 (let* (
        ;;(module '(nyacc lang c99 munge))
        (module '(ice-9 boot-9))
-       (dict (get-dict module))
-       (conns (get-conns dict))
-       (nodes (delete-duplicates (cons (caar dict) (map cdr conns))))
+       (depd (get-dict module))
+       (all (apply lset-union equal? depd))
+       (seq (reverse (tsort depd all)))
+       (scmfl (map
+               (lambda (m) (string-append
+                            (string-join (map symbol->string m) "/") ".scm"))
+               seq))
+       (scmpl (map %search-load-path scmfl))
+       (gopl (map compiled-file-name scmpl))
+       ;;(conns (get-conns dict))
+       ;;(nodes (delete-duplicates (cons (caar dict) (map cdr conns))))
        )
-  (pp dict)
+  ;;(pp scmfl)
+  ;;(pp scmpl)
+  (pp gopl)
+  #|
   (pp "====")
   (pp conns)
   (pp "====")
   (pp nodes)
+  |#
   #f)
-;;(load "tzort.scm")
-;;(use-modules (tzort))
-
-;;(define dct (get-dict '(nyacc lang c99 munge)))
-
-;;(define g1 '((u v x) (v y) (w y z) (x v) (y x) (z z)))
-;;(define lst (tzort g1))
-
-;;(pp lst)
 
 ;; --- last line ---
